363b0bce82582c5d70e9e778e5cd66351c1b894d
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-11 18:24:16 +0900

    コストの高いレンダリング

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,11 +1,17 @@
-export const ChildArea = (props) => {
-  const { open } = props;
 
-  const style = {
+const style = {
     width: "100%",
     height: "200px",
     backgroundColor: "khaki"
-  };
+};
+
+export const ChildArea = (props) => {
+  const { open } = props;
+  console.log("ChildAreaがレンダリングされた！！")
+  const data = [...Array(2000).keys()];
+  data.forEach(() =>{
+    console.log("....");
+  });
   return (
     <>
       {open ? (
